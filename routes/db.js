a1e96ee30d25b07d715722234fab1fd9a82ba17e
David Walsh <dawalsh@gmail.com>
2016-06-29 18:02:38 -0400

    Fix misspelled function name in db.js

--- routes/db.js
+++ routes/db.js
@@ -256,7 +256,7 @@ function getUpdatedFiles(uid,cb) {
 						cb(null, updatedFiles, db.access_token);
 					} else {
 						console.log("hasMore was true.. recursively calling getUpdatedFilesFromDropbox");
-						getUpdatedFileFromDropbox(cursor, updatedFiles);
+						getUpdatedFilesFromDropbox(cursor, updatedFiles);
 					}
 				});
 			}
